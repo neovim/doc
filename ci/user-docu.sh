b4ecb1caeeb47b11c89b0ebf60dda37bd610e9e4
Florian Walch <florian@fwalch.com>
2014-09-13 21:04:51 +0200

    Add user docu build.

---
+++ ci/user-docu.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -e
+
+BUILD_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+source ${BUILD_DIR}/ci/common/common.sh
+source ${BUILD_DIR}/ci/common/doc.sh
+source ${BUILD_DIR}/ci/common/neovim.sh
+source ${BUILD_DIR}/ci/common/html.sh
+
+generate_user_docu() {
+  require_environment_variable BUILD_DIR "${BASH_SOURCE[0]}" ${LINENO}
+  require_environment_variable MAKE_CMD "${BASH_SOURCE[0]}" ${LINENO}
+
+  cd ${NEOVIM_DIR}/runtime/doc
+  ${MAKE_CMD} html
+
+  # Copy to doc repository
+  rm -rf ${DOC_DIR}/user
+  mkdir -p ${DOC_DIR}/user
+  cp -r *.html ${DOC_DIR}/user
+
+  # Modify HTML to match Neovim's layout
+  modify_user_docu
+}
+
+# Helper function to modify user documentation HTML
+# to use Neovim layout
+modify_user_docu() {
+  for file in ${DOC_DIR}/user/*.html; do
+    local title="$(extract_title ${file})"
+    local body="$(echo "$(extract_body ${file})" | sed -e 's/color="purple"/color="#54A23D"/Ig')"
+    generate_report "${title}" "${body}" "${file}"
+  done
+}
+
+DOC_SUBTREE="/user/"
+clone_doc
+clone_neovim
+generate_user_docu
+commit_doc
