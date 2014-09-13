e72df59f811c1457f4c897ab1d466ca9de80109a
Florian Walch <florian@fwalch.com>
2014-09-13 22:18:52 +0200

    Improve vimpatch report styling.

--- ci/vimpatch-report.sh
+++ ci/vimpatch-report.sh
@@ -11,50 +11,42 @@ generate_vimpatch_report() {
   rm -rf ${DOC_DIR}/reports/vimpatch
   mkdir -p ${DOC_DIR}/reports/vimpatch
 
-  generate_report "vim-patch report" "$(get_vimpatch_report_body)" \
+  generate_report "Vim Patch Report" "$(get_vimpatch_report_body)" \
     ${DOC_DIR}/reports/vimpatch/index.html
 }
 
 get_vimpatch_report_body() {
-  get_open_pullrequests
-  get_version_c
+  # Get patch information from src/nvim/version.c
+  #   - merged patches:   listed in version.c
+  #   - unmerged patches: commented-out in version.c
+  #   - N/A patches:      commented-out with "//123 NA"
+  local patches=$(sed -n '/static int included_patches/,/}/p' ${NEOVIM_DIR}/src/nvim/version.c |
+    grep -e '[0-9]' | sed 's/[ ,]//g' | grep -ve '^00*$')
+
+  merged=$(echo "${patches}" | grep -v \/\/ | linkify_numbers) \
+  not_merged=$(echo "${patches}" | grep \/\/ | grep -v NA | sed 's/\/\///g' | linkify_numbers) \
+  not_applicable=$(echo "${patches}" | grep -e '\/\/.*NA' | sed 's/\/\/\|NA//g' | linkify_numbers) \
+  pull_requests=$(get_open_pullrequests) \
+  envsubst < ${BUILD_DIR}/templates/vimpatch-report/body.sh.html
 }
 
 # Decorates a list of numbers as links to Vim's online repo.
 linkify_numbers() {
   # zero-pad numbers less than 3 digits
   awk -F: '{ printf("%03d\n", $1) }' |
-  sed 's/[0-9]*/<a href="https:\/\/code.google.com\/p\/vim\/source\/detail?r=v7-4-\0">vim-patch:7.4.\0<\/a><br>/'
-}
-
-# Generate HTML report from src/nvim/version.c
-#   - merged patches:   listed in version.c
-#   - unmerged patches: commented-out in version.c
-#   - N/A patches:      commented-out with "//123 NA"
-get_version_c() {
-  local patches=$(sed -n '/static int included_patches/,/}/p' ${NEOVIM_DIR}/src/nvim/version.c |
-                  grep -e '[0-9]' | sed 's/[ ,]//g' | grep -ve '^00*$')
-
-  merged=$(echo "$patches" | grep -v \/\/ | linkify_numbers) \
-  not_merged=$(echo "$patches" | grep \/\/ | grep -v NA | sed 's/\/\///g' | linkify_numbers) \
-  not_applicable=$(echo "$patches" | grep -e '\/\/.*NA' | sed 's/\/\/\|NA//g' | linkify_numbers) \
-  envsubst < ${BUILD_DIR}/templates/vimpatch-report/body.sh.html
+  sed 's/[0-9]*/<li><a href="https:\/\/code.google.com\/p\/vim\/source\/detail?r=v7-4-\0">vim-patch:7.4.\0<\/a><\/li>/'
 }
 
 # Generate HTML report of the current 'vim-patch' pull requests on GitHub
 get_open_pullrequests() {
-  echo "<div class=\"col\"><h2>Pull requests</h2>"
-
-  curl 'https://api.github.com/repos/neovim/neovim/pulls?state=open&per_page=100' 2>/dev/null |
-  jq '[.[] | {html_url, title} |  select(contains({title: "vim-patch"}))] | sort_by(.title) | map("<a href=\"\(.html_url)\">\(.title)</a><br/>")' |
+  curl "https://api.github.com/repos/neovim/neovim/pulls?state=open&per_page=100" 2>/dev/null |
+  jq '[.[] | {html_url, title} |  select(contains({title: "vim-patch"}))] | sort_by(.title) | map("<li><a href=\"\(.html_url)\">\(.title)</a></li>")' |
   # use sed until travis gets jq 1.3+ (has 'reduce' and '@html')
   sed 's/^  "//' |
   sed 's/\("\|",\)$//' |
   sed 's/^\[//' |
   sed 's/^\]//' |
   sed 's/\\"/"/g'
-
-  echo "</div>"
 }
 
 is_ci_build && {
