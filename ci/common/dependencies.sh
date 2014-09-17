16f1140dd70fc8d97abbb5050f820b146fe2cbaf
Florian Walch <florian@fwalch.com>
2014-09-17 18:47:16 +0200

    Update clang version.

--- ci/common/dependencies.sh
+++ ci/common/dependencies.sh
@@ -3,7 +3,7 @@
 require_environment_variable BUILD_DIR "${BASH_SOURCE[0]}" ${LINENO}
 
 DOXYGEN_VERSION=${DOXYGEN_VERSION:-1.8.7}
-CLANG_VERSION=${CLANG_VERSION:-3.4}
+CLANG_VERSION=${CLANG_VERSION:-3.5}
 
 # Define directories where dependencies are installed to
 DEPS_INSTALL_DIR=${DEPS_INSTALL_DIR:-${BUILD_DIR}/build/.deps}
@@ -24,13 +24,14 @@ install_clang() {
   mkdir -p ${DEPS_BIN_DIR}
 
   echo "Installing Clang ${CLANG_VERSION}..."
-  sudo add-apt-repository 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise main'
+  sudo add-apt-repository "deb http://ppa.launchpad.net/ubuntu-toolchain-r/test/ubuntu precise main"
+  sudo add-apt-repository "deb http://llvm.org/apt/precise/ llvm-toolchain-precise-${CLANG_VERSION} main"
   wget -q -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
   sudo apt-get update -qq
   sudo apt-get install -y -q clang-${CLANG_VERSION}
 
-  ln -fs /usr/bin/clang ${DEPS_BIN_DIR}
-  ln -fs /usr/bin/scan-build ${DEPS_BIN_DIR}
+  ln -fs /usr/bin/clang-${CLANG_VERSION} ${DEPS_BIN_DIR}/clang
+  ln -fs /usr/bin/scan-build-${CLANG_VERSION} ${DEPS_BIN_DIR}/scan-build
 }
 
 # For info on jq, see https://stedolan.github.io/jq
