3a6f54cb6aef1c35bc89bf501073147d411949b8
Justin M. Keyes <justinkz@gmail.com>
2014-12-24 16:14:56 -0500

    deps: pass extra flags to cmake (fix x86 build)

--- ci/common/deps-repo.sh
+++ ci/common/deps-repo.sh
@@ -48,13 +48,13 @@ build_deps() {
   local depsdir="${!dir}${!subtree}"
   depsdir=${depsdir%/*}
   rm -rf ${depsdir}
-  mkdir -p ${depsdir}
+  mkdir ${depsdir}
 
   #build deps in non-default location...
   #  https://github.com/neovim/neovim/pull/1588#issuecomment-66778849
   cd ${depsdir}
-  cmake ${NEOVIM_DIR}/third-party/
-  make DEPS_CMAKE_FLAGS="-DDEPS_PREFIX=${depsdir} ${2}"
+  cmake -DDEPS_PREFIX=${depsdir} ${2} ${NEOVIM_DIR}/third-party/
+  make
 
   rm -rf ${depsdir}/{build,CMakeFiles,CMakeCache.txt,cmake_install.cmake,Makefile,.third-party}
 }
