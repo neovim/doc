fae91f981f8fd3d94b520945810bf803ddd3e071
Justin M. Keyes <justinkz@gmail.com>
2017-03-06 04:25:51 +0100

    ci/common/neovim.sh: clone with full depth
    We need the previous git tag so that the version can be generated precisely.

--- ci/common/neovim.sh
+++ ci/common/neovim.sh
@@ -9,7 +9,7 @@ NEOVIM_BRANCH=${NEOVIM_BRANCH:-master}
 clone_neovim() {
   if is_ci_build || ! [ -d ${NEOVIM_DIR} ] ; then
     rm -rf ${NEOVIM_DIR}
-    git clone --branch ${NEOVIM_BRANCH} --depth 1 git://github.com/${NEOVIM_REPO} ${NEOVIM_DIR}
+    git clone --branch ${NEOVIM_BRANCH} git://github.com/${NEOVIM_REPO} ${NEOVIM_DIR}
   else
     git --git-dir=${NEOVIM_DIR}/.git rev-parse HEAD >/dev/null \
       || exit 1
