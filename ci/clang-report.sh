b8d5ecd3ccaec3f1bbabf7b863e576be0727ac5a
ZyX <kp-pav@yandex.ru>
2017-05-02 03:13:23 +0300

    Refactor some reports: factor out badge generation and style changes

--- ci/clang-report.sh
+++ ci/clang-report.sh
@@ -3,12 +3,13 @@ set -e
 set -o pipefail
 
 BUILD_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
-source ${BUILD_DIR}/ci/common/common.sh
-source ${BUILD_DIR}/ci/common/dependencies.sh
-source ${BUILD_DIR}/ci/common/deps-repo.sh
-source ${BUILD_DIR}/ci/common/doc.sh
-source ${BUILD_DIR}/ci/common/neovim.sh
-source ${BUILD_DIR}/ci/common/html.sh
+source "$BUILD_DIR/ci/common/common.sh"
+source "$BUILD_DIR/ci/common/dependencies.sh"
+source "$BUILD_DIR/ci/common/deps-repo.sh"
+source "$BUILD_DIR/ci/common/doc.sh"
+source "$BUILD_DIR/ci/common/neovim.sh"
+source "$BUILD_DIR/ci/common/html.sh"
+source "$BUILD_DIR/ci/common/badge.sh"
 
 generate_clang_report() {
   cd ${NEOVIM_DIR}
@@ -73,15 +74,10 @@ modify_clang_report() {
 
 # Helper function to download clang analyzer badge from shields.io
 download_clang_badge() {
-  local all_bugs_number="$(find_all_bugs_number ${BUILD_DIR}/scan-build.out)"
-  local code_quality_color="$(get_code_quality_color ${all_bugs_number})"
-  local badge="clang_analysis-${all_bugs_number}-${code_quality_color}"
-  local response
-
-  response=$( 2>&1 curl --tlsv1 https://img.shields.io/badge/${badge}.svg \
-    > ${DOC_DIR}/reports/clang/badge.svg || true )
-  [ -f ${DOC_DIR}/reports/clang/badge.svg ] \
-    || echo "failed to download badge: $response"
+  download_badge \
+    "$(find_all_bugs_number "$BUILD_DIR/scan-build.out")" \
+    "clang_analysis" \
+    "$DOC_DIR/$DOC_SUBTREE"
 }
 
 # Helper function to find number of all bugs in build-scan output
@@ -92,24 +88,6 @@ find_all_bugs_number() {
   # 2. Substitute "No" by 0
   sed -n 's/scan-build: \(.*\) bugs\{0,1\} found./\1/p' ${1} \
     | sed 's/No/0/'
-}
-
-# Helper function to get the code quality color based on number of bugs
-# ${1}:   Number of all found bugs
-# Output: The name of the color
-get_code_quality_color() {
-  max_bugs=20
-  yellow_threshold=$(($max_bugs / 2))
-  bugs=$(($1 < $max_bugs ? $1 : $max_bugs))
-  if [[ $bugs -ge $yellow_threshold ]]; then
-    red=255
-    green=$((255 - 255 * ($bugs - $yellow_threshold) / $yellow_threshold))
-  else
-    red=$((255 * $bugs / $yellow_threshold))
-    green=255
-  fi
-  blue=0
-  printf "%.2x%.2x%.2x" $red $green $blue
 }
 
 DOC_SUBTREE="/reports/clang/"
