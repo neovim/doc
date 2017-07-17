8141e9578a4d183804fcf1a1bb041641b218e8d3
Dan Beste <Krakn@users.noreply.github.com>
2017-07-17 09:42:08 -0500

    doc-index.sh: lint (#115)
    - apply Google shell style guide: https://google.github.io/styleguide/shell
    - 'set -u': Treat unset variables as errors
    - readonly global variables
    - main() entrypoint

--- ci/doc-index.sh
+++ ci/doc-index.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env bash
+
 set -e
+set -u
 
-BUILD_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
-source ${BUILD_DIR}/ci/common/common.sh
-source ${BUILD_DIR}/ci/common/doc.sh
+readonly BUILD_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+readonly DOC_INDEX_PAGE_URL="${DOC_INDEX_PAGE_URL:-https://neovim.io/doc_index}"
+readonly DOC_SUBTREE='index.html'
 
-DOC_INDEX_PAGE_URL=${DOC_INDEX_PAGE_URL:-https://neovim.io/doc_index}
+source "${BUILD_DIR}/ci/common/common.sh"
+source "${BUILD_DIR}/ci/common/doc.sh"
 
 generate_doc_index() {
-  echo "Updating index.html from ${DOC_INDEX_PAGE_URL}."
-  curl -L --tlsv1 ${DOC_INDEX_PAGE_URL} > ${DOC_DIR}/index.html
+  echo "Updating index.html from: ${DOC_INDEX_PAGE_URL}"
+  curl -L --tlsv1 "${DOC_INDEX_PAGE_URL}" -o "${DOC_DIR}/${DOC_SUBTREE}"
 }
 
-DOC_SUBTREE="/index.html"
-clone_doc
-generate_doc_index
-commit_doc
+main() {
+  clone_doc
+  generate_doc_index
+  commit_doc
+}
+
+main
