a21b27cf9f5c5d762f15bc74e367d4af58a5c744
Justin M. Keyes <justinkz@gmail.com>
2019-01-26 01:43:51 +0100

    sync-mirrors.sh: --mirror all branches

--- ci/common/common.sh
+++ ci/common/common.sh
@@ -178,7 +178,7 @@ can_fail_without_private() {
 commit_subtree() {
   (
     local prefix="${1}"
-    local attempts="${2:-10}"
+    local attempts="${2:-4}"
     local push_args="${3:-}"
     local subtree="${prefix}_SUBTREE"
     local dir="${prefix}_DIR"
@@ -191,7 +191,9 @@ commit_subtree() {
     require_environment_variable "${subtree}" "${BASH_SOURCE[0]}" ${LINENO}
     require_environment_variable "${dir}" "${BASH_SOURCE[0]}" ${LINENO}
     require_environment_variable "${repo}" "${BASH_SOURCE[0]}" ${LINENO}
-    require_environment_variable "${branch}" "${BASH_SOURCE[0]}" ${LINENO}
+    if ! echo "${push_args}" | >/dev/null 2>&1 grep -- '--all\|--mirror' ; then
+      require_environment_variable "${branch}" "${BASH_SOURCE[0]}" ${LINENO}
+    fi
 
     cd "${!dir}" || return
 
@@ -205,14 +207,14 @@ commit_subtree() {
       git commit -m "${CI_TARGET//-/ }: Automatic update" || true
 
       while test $(( attempts-=1 )) -ge 0 ; do
-        if echo "${push_args}" | >/dev/null 2>&1 grep -- '--force' \
+        if echo "${push_args}" | >/dev/null 2>&1 grep -- '--force\|--all\|--mirror' \
             || git pull --rebase "git://github.com/${!repo}" "${!branch}" ; then
           if ! has_gh_token ; then
             log_info 'GH_TOKEN not set; push skipped'
             log_info 'To test pull requests, see instructions in README.md'
             return "$(can_fail_without_private)"
           fi
-          if git push ${push_args} "https://github.com/${!repo}" "${!branch}"
+          if git push ${push_args} "https://github.com/${!repo}" ${!branch}
           then
             log_info "Pushed to: ${!repo} ${!branch}"
             return 0
@@ -225,10 +227,10 @@ commit_subtree() {
       done
       return 1
     else
-      if prompt_key_local "Build finished, do you want to commit and push the results to ${!repo}:${!branch} (change by setting ${repo}/${branch})?" ; then
-      # Commit in local builds.
+      if prompt_key_local "Build finished; commit and push to ${!repo}:${!branch:-*} ? (change by setting ${repo}/${branch})" ; then
+        # Commit in local builds.
         git commit || true
-        git push "ssh://git@github.com/${!repo}" "${!branch}"
+        git push ${push_args} "https://github.com/${!repo}" ${!branch}
       fi
     fi
   )
--- ci/sync-mirrors.sh
+++ ci/sync-mirrors.sh
@@ -12,11 +12,10 @@ MIRROR_BRANCH=${MIRROR_BRANCH:-master}
 
 sync_git_mirror() {
   local repo="${1}"
-  local branch="${2:-${MIRROR_BRANCH}}"
   local upstream_url="${3}"
   local repo_dir="${WORK_DIR}/${repo}"
 
-  echo "Cloning into ${repo_dir} from: ${upstream_url}"
+  echo "Cloning: ${upstream_url}"
   rm -rf "${repo_dir}"
   git clone "${upstream_url}" "${repo_dir}"
 
@@ -24,8 +23,8 @@ sync_git_mirror() {
   MIRROR_SUBTREE="/" \
   MIRROR_DIR="${repo_dir}" \
   MIRROR_REPO="${MIRROR_USER}/${repo}" \
-  MIRROR_BRANCH="${branch}" \
-    commit_subtree MIRROR
+  MIRROR_BRANCH= \
+    commit_subtree MIRROR 4 --mirror
 }
 
 sync_bzr_mirror() {
@@ -33,7 +32,7 @@ sync_bzr_mirror() {
   local upstream_url="http://bazaar.leonerd.org.uk/c/${repo}"
   local repo_dir="${WORK_DIR}/${repo}"
 
-  echo "Cloning upstream repo ${upstream_url} into ${repo_dir}"
+  echo "Cloning: ${upstream_url}"
   rm -rf "${repo_dir}"
   git clone "bzr::${upstream_url}" "${repo_dir}"
 
@@ -41,7 +40,8 @@ sync_bzr_mirror() {
   MIRROR_SUBTREE="/" \
   MIRROR_DIR="${repo_dir}" \
   MIRROR_REPO="${MIRROR_USER}/${repo}" \
-    commit_subtree MIRROR
+  MIRROR_BRANCH= \
+    commit_subtree MIRROR 4 --mirror
 }
 
 sync_bzr_mirror libvterm
