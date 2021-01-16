51170a1b41aae1969e90760b51bb36d335d35c78
James McCoy <jamessan@jamessan.com>
2021-01-15 20:54:12 -0500

    ci: Use state of local repo instead of remote when truncating history

--- ci/common/doc.sh
+++ ci/common/doc.sh
@@ -8,15 +8,10 @@ DOC_BRANCH=${DOC_BRANCH:-gh-pages}
 
 # Keep the https://github.com/neovim/doc/ repository history trimmed, otherwise
 # it gets huge and slow to clone.  We don't care about its commit history.
-#
-# NOTE: Do this after ALL other reports were pushed, otherwise their
-#       respective commit_subtree ("git pull --rebase") steps will fail.
 try_truncate_history() {
   cd "${DOC_DIR}" || { log_error "try_truncate_history: cd failed"; exit 1; }
   local branch=gh-pages
   if NEW_ROOT=$(2>/dev/null git rev-parse "$branch"~11) ; then
-    git fetch https://github.com/"${DOC_REPO}" "$branch"
-    git reset --hard FETCH_HEAD
     git_truncate "$branch" "$branch"~10
   else
     log_info "try_truncate_history: branch ${branch} has too few commits, skipping truncate"
