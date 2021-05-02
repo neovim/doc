c428c46c784244733929363cb5638845cac974b9
Matthieu Coudron <mcoudron@hotmail.com>
2021-05-02 18:50:18 +0200

    ci: fetch doc from neovim.io/doc2
    ideally this would be recursive but for now I hardcoded the files

--- ci/doc-index.sh
+++ ci/doc-index.sh
@@ -4,15 +4,14 @@ set -e
 set -u
 
 readonly BUILD_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
-readonly DOC_INDEX_PAGE_URL="${DOC_INDEX_PAGE_URL:-https://neovim.io/doc_index}"
 readonly DOC_SUBTREE='index.html'
 
 source "${BUILD_DIR}/ci/common/common.sh"
 source "${BUILD_DIR}/ci/common/doc.sh"
 
 generate_doc_index() {
-  echo "Updating index.html from: ${DOC_INDEX_PAGE_URL}"
-  curl -L --tlsv1 "${DOC_INDEX_PAGE_URL}" -o "${DOC_DIR}/${DOC_SUBTREE}"
+  echo "Updating doc folder from file-list.txt"
+  wget -i file-list.txt --force-directories -nH --cut-dirs=1 -P "${DOC_DIR}"
 }
 
 main() {
