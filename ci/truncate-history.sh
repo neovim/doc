183bd23b9f0b5c11d45235aaba175360fca935fb
dundargoc <33953936+dundargoc@users.noreply.github.com>
2023-11-11 16:04:07 +0100

    Fix typo (#48)

--- ci/truncate-history.sh
+++ ci/truncate-history.sh
@@ -11,7 +11,7 @@ DOC_DIR=${DOC_DIR:-${BUILD_DIR}/build/doc}
 DOC_REPO=${DOC_REPO:-neovim/doc}
 DOC_BRANCH=${DOC_BRANCH:-gh-pages}
 
-# Trim the https://github.com/neovim/doc/ repository history, els it gets huge
+# Trim the https://github.com/neovim/doc/ repository history, else it gets huge
 # and slow to clone.  We don't care about the history of the `gh-pages` branch.
 try_truncate_history() {
   cd "${DOC_DIR}" || { log_error "try_truncate_history: cd failed"; exit 1; }
