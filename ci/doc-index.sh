60dfb7a573afc0632a19876a4377e7e36dfb1c31
Justin M. Keyes <justinkz@gmail.com>
2025-11-25 00:44:27 -0500

    cleanup

--- ci/doc-index.sh
+++ ci/doc-index.sh
@@ -8,7 +8,7 @@ readonly BUILD_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 source "${BUILD_DIR}/ci/common/common.sh"
 
 generate_doc_index() {
-  echo "Updating doc folder from file-list.txt"
+  echo "Updating doc/ from file-list.txt"
   wget -i "${BUILD_DIR}/ci/file-list.txt" --force-directories -nH --cut-dirs=1 -P "${DOC_DIR}"
 }
 
